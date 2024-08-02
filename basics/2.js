01f3d00ce3ea55b545d2d409fed594c0b4bd2380
Hardik Sharma <123174149+Hardik-S-11@users.noreply.github.com>
2024-08-02 19:27:54 +0000

    learning basics

---
+++ basics/2.js
@@ -0,0 +1,15 @@
+const accountID = 21048
+let accountEmail = "hardik@sharma.com"
+var accountPassword = "Hardik123"
+accountCity = "Jaipur"
+let accountState
+
+// accountID = 54321
+
+// console.log(accountID)
+
+accountEmail = "sharma@hardik.com"
+accountPassword = "Sharma123"
+accountCity = "Bengaluru"
+
+console.table([accountID, accountEmail, accountCity, accountPassword, accountState])
