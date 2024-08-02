5e0eb9fe0e39145268570a4f9531398430cf9e51
Hardik Sharma <123174149+Hardik-S-11@users.noreply.github.com>
2024-08-02 19:47:29 +0000

    practise

---
+++ basics/2_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //is a keyword to force the browser to use the latest version of the language
+
+// alert("Stop") // for browser to prompt alerts 
+
+let name = "Hardik"
+let age = 21
+let isMale = true 
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+// objects
+
+console.log(typeof null);
+console.log(typeof undefined);
+
